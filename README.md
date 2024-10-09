6ebce05c131379cdb6ef727380c69b5b191718de
olaf <juliendupdupdup@gmail.com>
2024-10-09 18:52:32 +0100

    Implemented full functionality

---
+++ README.md
@@ -0,0 +1,34 @@
+## Master React Native Authentication with Expo Router & Clerk [📺](https://youtu.be/zh6Sc1flK2g)
+
+#### by Simon Grimm from [Galaxies_dev](https://www.youtube.com/@galaxies_dev) YouTube channel
+
+<!-- ### 🧠 Concepts Covered:
+
+- Expo React Native framework
+- NativeWind
+- Sending Data
+- Rendering HTML
+- Routers
+- Advanced Routing
+- Middleware
+- Rendering Static Files
+- Parsing Form/JSON Data
+- Parse Query Params-->
+
+### 🖥 Terminal commands used
+
+#### Create Expo template @1:10
+
+`npx create-expo-app@latest clerkApp --template tabs ./`
+
+#### Install Clerk @4:32
+
+`npm install @clerk/clerk-expo`
+
+#### Install Clerk Secure Store @4:46
+
+`npm install expo-secure-store`
+
+#### Install Loading Spinner @4:55
+
+`npm install react-native-loading-spinner-overlay`
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,59 +1,66 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
+import { Slot, useRouter, useSegments } from "expo-router";
+import { useEffect } from "react";
+import * as SecureStore from "expo-secure-store";
 
-import { useColorScheme } from '@/components/useColorScheme';
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
-
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      const item = await SecureStore.getItemAsync(key);
+      if (item) {
+        console.log(`${key} was used 🔐 \n`);
+      } else {
+        console.log("No values stored under key: " + key);
+      }
+      return item;
+    } catch (error) {
+      console.error("SecureStore get item error: ", error);
+      await SecureStore.deleteItemAsync(key);
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
 };
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
-
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+const InitialLayout = () => {
+  const { isLoaded, isSignedIn } = useAuth();
+  const segments = useSegments();
+  const router = useRouter();
 
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    if (!isLoaded) return;
+
+    const inTabsGroup = segments[0] === "(auth)";
+
+    console.log("User changed: ", isSignedIn);
+
+    if (isSignedIn && !inTabsGroup) {
+      router.replace("/home");
+    } else if (!isSignedIn) {
+      router.replace("/login");
     }
-  }, [loaded]);
+  }, [isSignedIn]);
 
-  if (!loaded) {
-    return null;
-  }
+  return <Slot />;
+};
 
-  return <RootLayoutNav />;
-}
-
-function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
+const RootLayout = () => {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
+    <ClerkProvider
+      publishableKey={CLERK_PUBLISHABLE_KEY}
+      tokenCache={tokenCache}
+    >
+      <InitialLayout />
+    </ClerkProvider>
   );
-}
+};
+
+export default RootLayout;
